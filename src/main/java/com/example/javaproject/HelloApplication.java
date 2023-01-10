3b4fc14cd5c43a031715fb4f2eea0ffd3e5ce31d
rinsarmu <roberaensermu@gmail.com>
2023-01-10 14:32:32 +0300

    Adding childrens to their parents and updating scene windowsnm

--- src/main/java/com/example/javaproject/HelloApplication.java
+++ src/main/java/com/example/javaproject/HelloApplication.java
@@ -66,7 +66,7 @@ public class HelloApplication extends Application {
         MenuItem replace = new MenuItem("Exit");
 
         // --- EDIT Item
-        MenuItem item1 = new MenuItem("Open");
+        MenuItem open = new MenuItem("Open");
         MenuItem item2 = new MenuItem("Save");
         MenuItem item3 = new MenuItem("Save as");
         MenuItem item4 = new MenuItem("Exit");
@@ -91,7 +91,7 @@ public class HelloApplication extends Application {
         buttons.getChildren().addAll(login, exit);
         layout1.getChildren().addAll(errorUserName, userText, errorPassword, passText);
         boxFields.getChildren().addAll(layout1, buttons);
-        menuFile.getItems().addAll(item1, item2, item3, item4);
+        menuFile.getItems().addAll(open, item2, item3, item4);
         menuEdit.getItems().addAll(undo,cut,copy,paste,delete);
         menuBar.getMenus().addAll(menuFile,menuEdit, menuView);
         layout2.getChildren().addAll(menuBar, editText);
@@ -137,10 +137,224 @@ public class HelloApplication extends Application {
 
 //===========[ Action start => ]===========
 
+
+
+        login.setOnMouseEntered(e->{
+            login.setEffect(dropShadow);
+        });
+        login.setOnMouseExited(e->{
+            login.setEffect(null);;
+        });
+
+        exit.setOnMouseEntered(e->{
+            exit.setEffect(dropShadow);
+        });
+        exit.setOnMouseExited(e->{
+            exit.setEffect(null);;
+        });
+
+        userText.setOnMouseEntered(e->{
+            userText.setEffect(dropShadow);
+        });
+        userText.setOnMouseExited(e->{
+            userText.setEffect(null);;
+        });
+
+        passText.setOnMouseEntered(e->{
+            passText.setEffect(dropShadow);
+        });
+        passText.setOnMouseExited(e->{
+            passText.setEffect(null);;
+        });
+
+        //Authentication
+        login.setOnAction(e -> {
+            String check = userText.getText();
+            numeric = check.matches("-?\\d+(\\.\\d+)?");
+            System.out.println(numeric);
+            if (numeric) {
+                if (phoneNumber.equals(check)) {
+                    userText.setStyle("-fx-border-color: #dddfe2;");
+                    errorUserName.setText("");
+                    userFlag = true;
+                } else {
+                    System.out.println("Incorrect.");
+                    errorUserName.setText("Error in phone number");
+                    window.setScene(scene1);
+                    userFlag = false;
+                    userText.setStyle("-fx-border-color: #red;");
+
+                }
+            } else {
+                if (user.equals(check)) {
+                    errorUserName.setText("");
+                    userFlag = true;
+                    userText.setStyle("-fx-border-color: #dddfe2;");
+
+                } else {
+                    System.out.println("Incorrect.");
+                    errorUserName.setText("Error in username");
+                    window.setScene(scene1);
+                    userFlag = false;
+                    userText.setStyle("-fx-border-color: red;");
+
+                }
+            }
+
+            if (pswd.equals(passText.getText())) {
+                errorPassword.setText("");
+                passFlag = true;
+                passText.setStyle("-fx-border-color: #dddfe2;");
+
+            } else {
+                System.out.println("Incorrect.");
+                errorPassword.setText("Error in password");
+                window.setScene(scene1);
+                passFlag = false;
+                passText.setStyle("-fx-border-color: red;");
+
+            }
+            if (passFlag && userFlag) {
+                System.out.println("Logged in");
+                System.out.println(check);
+                window.setScene(scene2);
+                window.setTitle("Untitled - Notepad");
+            } else {
+                System.out.println("UserName doesnot exist");
+            }
+        });
+
+        //Leave the System
+        exit.setOnAction(e -> {
+            System.exit(0);
+        });
+
+        // Open Action
+        open.setOnAction(e->{
+
+            // File system;
+            fileChooser.setTitle("Open Resource FIle");
+            if(fileLocation.isBlank()){
+                fileChooser.setInitialDirectory(new File("/home/kuusaa/Documents"));
+
+            } else {
+                fileChooser.setInitialDirectory(new File(fileLocation));
+
+            }
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("pdf Files", "*.pdf")
+            );
+
+            File selectedFile = fileChooser.showOpenDialog(window);
+            String line;
+
+            if (selectedFile != null) {
+                fileName = selectedFile.getName();
+                fileLocation = selectedFile.getAbsolutePath();
+                window.setTitle(fileName + " - Notepad");
+                editText.setText(" ");
+                System.out.println(selectedFile.getAbsoluteFile());
+                BufferedReader reader = null;
+                try {
+                    reader = new BufferedReader(new FileReader(selectedFile));
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                while (true) {
+                    try {
+                        if (!((line = reader.readLine()) != null)) break;
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    System.out.println(line);
+                    //                editText.setText((line));
+                    editText.appendText(line);
+                }
+
+            }
+            System.out.println("Saved");
+
+        });
+
+
+        //        Save Action
+        item2.setOnAction(e->{
+            String sampleText = editText.getText();
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+            fileChooser.getExtensionFilters().add(extFilter);
+
+            System.out.println("Loca"+ fileLocation);
+            File file;
+
+            if (fileLocation.isBlank()){
+                System.out.println("not saved beore");
+                file = fileChooser.showSaveDialog(window);
+                System.out.println(file.getAbsoluteFile());
+                fileLocation = file.getAbsolutePath();
+                if(file != null){
+                    try {
+                        saveTextToFile(sampleText, file);
+                    } catch (FileNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            } else {
+                file = new File(fileLocation);
+                System.out.println("Not empty");
+                try {
+                    saveTextToFile(sampleText, file);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+            }
+
+        });
+
+        //Save As Action
+        item3.setOnAction(e->{
+            String sampleText = editText.getText();
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+            fileChooser.getExtensionFilters().add(extFilter);
+
+            System.out.println("Loca"+ fileLocation);
+            File  file = fileChooser.showSaveDialog(window);
+            System.out.println("save as");
+
+
+            if(file != null){
+                try {
+                    fileLocation = file.getAbsolutePath();
+                    saveTextToFile(sampleText, file);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
+
+        });
+
+        //Exit Action
+        item4.setOnAction(e -> {
+            System.exit(0);
+
+        });
+
+
 //===========[ Action END <= ]===========
 
 
 
+
+    }
+
+    private void saveTextToFile(String sampleText, File file) throws FileNotFoundException {
+        System.out.println("sample :" + sampleText);
+        PrintWriter writer = new PrintWriter(file);
+        writer.println(sampleText);
+        writer.close();
     }
 
     public static void main(String[] args) {
